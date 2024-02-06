7bfcf0487747792909e0cb232984356881798991
Anshuman Dubey <anshumandubey1609@gmail.com>
2024-02-06 20:58:15 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-const mrng = "Good Morning";
-const after = "Good Morning";
-const ngt = "Good Morning";
+
+let currDate = new Date(2024, 5, 5, 20);
+currDate = currDate.getHours();
+let greet = '';
+const cStyle = {};
+
+if(currDate >= 1 && currDate < 12){
+  greet = 'Good Morning';
+  cStyle.color  = 'green';
+}
+else if(currDate >= 12 && currDate < 19){
+  greet = 'Good Afternoon';
+  cStyle.color  = 'Orange';
+}
+else{
+  greet = 'Good Night';
+  cStyle.color  = 'black';
+}
 
 ReactDOM.render(
   <>
-    <h1>Hello Sir, </h1>
+    <div>
+      <h1>Hello Sir, <span style={cStyle}> {greet} </span></h1>
+    </div>
   </>
   ,document.getElementById('root')
 );
