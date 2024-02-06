3a8592d4dcdf8a9fa0110feb829825ef63ec79a1
Anshuman Dubey <anshumandubey1609@gmail.com>
2024-02-06 20:19:36 +0530

    commit new

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const mrng = "Good Morning";
+const after = "Good Morning";
+const ngt = "Good Morning";
+
+ReactDOM.render(
+  <>
+    <h1>Hello Sir, </h1>
+  </>
+  ,document.getElementById('root')
+);
